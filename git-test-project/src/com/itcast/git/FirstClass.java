d22d4f5ca4da62f8f86971f204756f556a5056d0
kkk <kkk@163.com>
2020-07-23 22:19:45 +0800

    second add commit

--- git-test-project/src/com/itcast/git/FirstClass.java
+++ git-test-project/src/com/itcast/git/FirstClass.java
@@ -3,5 +3,6 @@ package com.itcast.git;
 public class FirstClass {
     public static void main (String[] args){
         System.out.println("hello project");
+        System.out.println("hello2 project");
     }
 }
